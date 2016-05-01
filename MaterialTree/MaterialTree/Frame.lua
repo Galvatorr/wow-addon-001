34d7a5b5ac99ffdab6ade93d42b8fd26d46e3dec
Galvatorr Sukoshika <Galvatorr@gmail.com>
2016-05-01 01:19:49 -0400

    Both components work
    Still need to
    1) Only show leaves on the top
    2) scrolling for too much text

--- MaterialTree/MaterialTree/Frame.lua
+++ MaterialTree/MaterialTree/Frame.lua
@@ -1,7 +1,12 @@
 -- Author      : Jared
 -- Create Date : 4/30/2016 16:43:47
 -- ChatFrame1:AddMessage
--- BuildListString
+
+function MaterialFrame_OnLoad()
+	MaterialText:SetText("1) Select recipe\n2) Enter number for Make X\n3) Click Update button");
+	MaterialFrame:RegisterEvent("TRADE_SKILL_SHOW");
+	MaterialFrame:RegisterEvent("TRADE_SKILL_CLOSE");
+end
 
 function MaterialFrame_OnEvent(self, event, ...)
 	if event == "TRADE_SKILL_SHOW" then
@@ -33,60 +38,83 @@ function UpdateButton_OnClick()
 		end
 	end
 
+	-- Get material tree
+	local materialTreeText, materialNeeded, materialOwned, materialLink = GetMaterialTree(skillIndex, numberToMake, "");
+
+	-- Create material diff text
+	local materialDiffText = "";
+	for reagentName, materialNeededAmount in pairs(materialNeeded) do
+		local materialDiff = materialNeededAmount - materialOwned[reagentName] < 0 and 0 or materialNeededAmount - materialOwned[reagentName];
+		materialDiffText = materialDiffText .. materialDiff .. " " .. materialLink[reagentName] .. "\n";
+	end
+
 	-- Set text
-	MaterialText:SetText("Remaining Materials for " .. numberToMake .. " " .. GetTradeSkillItemLink(skillIndex) .. "\n\nnum stuff remaining\n\n\nTree View of Total Materials\n" .. GetMaterialTreeText(skillIndex, numberToMake, ""));
+	MaterialText:SetText(numberToMake .. " " .. GetTradeSkillItemLink(skillIndex) .. "\n\n\nList of Remaining Materials" .. "\n\n" .. materialDiffText .. "\n\nTree View of Total Materials Needed\n" .. materialTreeText);
 end
 
 function ExpandAllHeaders()
 	while true do
+		-- Assume we will break out unless we have to expand
 		local breakOut = true;
 		for curSkillIndex = 1,GetNumTradeSkills() do 
 			local _, skillType, _, isExpanded = GetTradeSkillInfo(curSkillIndex)
+
+			-- If we found a non expanded header, expand it and start over
 			if ((skillType == "header") or (skillType == "subheader")) and (not isExpanded) then
 				ExpandTradeSkillSubClass(curSkillIndex);
 				breakOut = false;
 				break;
 			end
 		end
+
+		-- No more expansions were performed, so break out
 		if breakOut then
 			break;
 		end
 	end
 end
 
-function GetMaterialTreeText(skillIndex, numberToMake, spaces)
-	-- Initialize materialTreeText
+function GetMaterialTree(skillIndex, numberToMake, spaces)
+	-- Initialize variables
 	if spaces == "" then
 		materialTreeText = "";
+		materialNeeded = {};
+		materialOwned = {};
+		materialLink = {};
 	end
 
-	-- Go through each reagent
 	for reagentIndex = 1,GetTradeSkillNumReagents(skillIndex) do
-		-- Get some properties and multiply the reagentCount by how many to make
-		local link = GetTradeSkillReagentItemLink(skillIndex, reagentIndex);
+		-- Get some properties
+		local reagentLink = GetTradeSkillReagentItemLink(skillIndex, reagentIndex);
 		local reagentName, _, reagentCount, playerReagentCount = GetTradeSkillReagentInfo(skillIndex, reagentIndex);
+
+		-- Multiply the reagentCount by how many to make
 		local reagentCount = reagentCount * numberToMake;
-		
-		-- FIX THIS
-		--local reagentDiff = reagentCount - playerReagentCount < 0 and 0 or reagentCount - playerReagentCount;
+
+		-- Initialize this material
+		if materialNeeded[reagentName] == nil then 
+			materialNeeded[reagentName] = 0;
+		end
+
+		-- Update global material lists
+		materialNeeded[reagentName] = materialNeeded[reagentName] + reagentCount;
+		materialOwned[reagentName] = playerReagentCount;
+		materialLink[reagentName] = reagentLink;
 		
 		-- Set text
-		materialTreeText = materialTreeText .. "\n" .. spaces .. reagentCount .. " " .. link;
+		materialTreeText = materialTreeText .. "\n" .. spaces .. reagentCount .. " " .. reagentLink;
 
 		-- Recurse only if we can make this item
 		for curSkillIndex = 1,GetNumTradeSkills() do 
 			if GetTradeSkillInfo(curSkillIndex) == reagentName then
-				GetMaterialTreeText(curSkillIndex, reagentCount, spaces .. "        ");
+				GetMaterialTree(curSkillIndex, reagentCount, spaces .. "        ");
 				break;
 			end
 		end
 	end
 
-	return materialTreeText;
-end
-
-function MaterialFrame_OnLoad()
-	MaterialText:SetText("1) Select recipe\n2) Enter number for Make X\n3) Click Update button");
-	MaterialFrame:RegisterEvent("TRADE_SKILL_SHOW");
-	MaterialFrame:RegisterEvent("TRADE_SKILL_CLOSE");
+	-- Return variables
+	if spaces == "" then
+		return materialTreeText, materialNeeded, materialOwned, materialLink;
+	end
 end
